2a9982d9c3744c895748545bcb855b9fdbfa3211
sam-u1 <sasilipi@ucs.br>
2025-11-26 00:12:37 -0300

    Update script.js

--- Trabalho Web/script.js
+++ Trabalho Web/script.js
@@ -70,4 +70,70 @@ document.getElementById('form-contato').addEventListener('submit', function(even
             		telefone
         	};
 
-		const index = users.findIndex(
+		const index = users.findIndex(u => u.id === userAtualizado.id);
+		
+		users[index] = userAtualizado;
+	
+		const linhas = document.querySelectorAll('#lista-contatos tr');
+		linhas.forEach(linha => {
+			const botaoEditar = linha.querySelector('.editar');
+			if (parseInt(botaoEditar.classList[1]) === parseInt(userAtualizado.id)) {
+				linha.children[0].textContent = userAtualizado.nome;
+				linha.children[1].textContent = userAtualizado.email;
+				linha.children[2].textContent = userAtualizado.telefone;
+			}
+		});
+		
+		document.getElementById('form-contato').reset();
+		document.getElementById('btn-submit').textContent= 'Cadastrar';
+        	localStorage.setItem(idEditar, JSON.stringify(userAtualizado));
+        	idEditar = null;
+        	return;
+    	}
+
+  	document.getElementById('form-contato').reset();
+   
+   	const linha = document.createElement('tr');
+
+   	document.getElementById('lista-contatos').appendChild(linha);
+   	linha.appendChild(document.createElement('td')).textContent = nome;
+   	linha.appendChild(document.createElement('td')).textContent = email;
+   	linha.appendChild(document.createElement('td')).textContent = telefone;
+  
+   	const botao = document.createElement('button');
+   	botao.classList.add('btn-criado',id,'editar');
+   	botao.id = 'editar';
+   	botao.textContent = 'Editar';
+   	linha.appendChild(botao);
+   
+   	const botaoEx = document.createElement('button');
+   	botaoEx.classList.add('btn-criado',id,'excluir');
+   	botaoEx.id = 'excluir';
+   	botaoEx.textContent = 'Excluir';
+   	linha.appendChild(botaoEx);
+   
+
+   	let userObjeto ={
+		id: id,
+		nome: nome,
+		email: email,
+		telefone: telefone
+   	};
+	users.push(userObjeto);
+   	id++;
+   	const usuarioStringJSON = JSON.stringify(userObjeto);
+  
+   	localStorage.setItem(userObjeto.id, usuarioStringJSON);
+});
+
+carrega();
+
+document.getElementById('busca').addEventListener('change', function(event){
+
+	const busca = users.filter(user => user.nome.toLowerCase().includes(document.getElementById('busca').value.toLowerCase()) || user.email.toLowerCase().includes(document.getElementById('busca').value.toLowerCase()));
+	if(busca){
+		const linhas = document.querySelectorAll('#lista-contatos tr');
+		linhas.forEach(linha => linha.remove());
+		busca.forEach(user => Lista(user));
+	}
+});
